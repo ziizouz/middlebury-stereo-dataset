325d10a769d8b1c90faa0a1068c4e242e1ac2c52
Ziizou ZIDANE <wizouzizou@yahoo.fr>
2019-03-31 12:31:12 +0300

    Gray conversion and save into pickle added !

--- dataset_maker.py
+++ dataset_maker.py
@@ -2,6 +2,7 @@ import os
 import glob
 import cv2
 import numpy as np
+import pickle
 
 # Read links from txt file
 links_file_name = "dataset_links.txt"
@@ -25,12 +26,13 @@ zip_names = glob.glob('*.zip')
 
 for filename in zip_names:
 	os.system('unzip ' + filename) # unzipping
-	os.system('rm ' + filename) # removing zip file to save space
+	#os.system('rm ' + filename) # removing zip file to save space
 
 # Converting PFM to PNG 
 filenames = glob.glob('*-perfect') # All unzip filenames
-
+print(filenames)
 for filename in filenames:
+
 	# Removing -sd.pfm since I am not interested in those
 	sd_pfm_files = glob.glob(filename + '/*-sd.pfm')
 	for sd_pfm_file in sd_pfm_files:
@@ -47,4 +49,34 @@ for filename in filenames:
 	pgm_files = glob.glob(filename + '/*.pgm')
 	for pgm_file in pgm_files:
 		os.system('rm ' + pgm_file)
+	# removing lightning images (E & L) since I am not interested in those
+	lightning_file = glob.glob(filename + '/*1L.png')
+	os.system('rm ' + lightning_file[0])
+	lightning_file = glob.glob(filename + '/*1E.png')
+	os.system('rm ' + lightning_file[0])
 
+## Creation of the pickle
+depth_imgs = []
+gray_imgs  = []
+
+for filename in filenames:
+	png_depth_files = glob.glob(filename + '/disp*.png')
+	png_im_files = glob.glob(filename + '/im*.png')
+	for png_depth_file, png_im_file  in zip(png_depth_files, png_im_files):
+		# Adding depth file
+		depth = cv2.imread(png_depth_file, 0) # read as gray img
+		depth_imgs.append(depth)
+		# Adding im file as gray
+		im = cv2.imread(png_im_file, 0) # read as gray image
+		gray_imgs.append(im)
+
+# File lists are ready, let's save them into a pickle
+# Saving depth images
+with open('depth.pickle', "wb") as pickle_out:
+	pickle.dump(depth_imgs, pickle_out)
+
+# Saving gray images
+with open('gray.pickle', "wb") as pickle_out:
+	pickle.dump(gray_imgs, pickle_out)
+
+# Done
