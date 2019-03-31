701c8fd089b41dc17fd2713f23207478fe144087
Ziizou ZIDANE <wizouzizou@yahoo.fr>
2019-03-31 11:44:54 +0300

    unzipper implemented

--- dataset_maker.py
+++ dataset_maker.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import cv2
 import numpy as np
 
@@ -16,5 +17,10 @@ with open(links_file_name, 'r') as links_file:
 		# download file 
 		ret = os.system('wget -c ' + link)
 
+# unzipping all downloaded file
+# Getting all filenames
+zip_names = glob.glob('*.zip')
 
-
+for filename in zip_names:
+	os.system('unzip ' + filename) # unzipping
+	os.system('rm ' + filename) # removing zip file to save space
